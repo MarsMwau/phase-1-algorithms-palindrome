e8ab1ba890d31ea88173a2b2dc35d76007942d13
MarsMwau <marthamumbua16@gmail.com>
2023-03-31 01:19:13 +0300

    Complete

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+   // Convert the string to lowercase and remove non-letter characters
+   word = word.toLowerCase().replace(/[^a-z]/g, '');
+  
+   // Check if the string is equal to its reverse
+   return word === word.split('').reverse().join('');
+
 }
 
 /* 
   Add your pseudocode here
+   // Convert the string to lowercase and remove non-letter characters
+   word = word.toLowerCase().replace(/[^a-z]/g, '');
+  
+   // Check if the string is equal to its reverse
+   return word === word.split('').reverse().join('');
+
+
 */
 
 /*
   Add written explanation of your solution here
+1. Remove any spaces or punctuation from the input word.
+2. Start at the beginning and end of the word.
+3. Compare the letters at the beginning and end of the word. If they are different, the word is not a palindrome.
+4. Move one step closer to the middle of the word, and compare the letters again. Keep doing this until the pointers meet in the middle of the word, or until you find two letters that don't match.
+5. If you make it all the way through the word without finding two letters that don't match, then the word is a palindrome.
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
